dc73342c6aefd32146a88075502caf598ec4204f
Piper616 <f.diazmendez@gmail.com>
2021-10-03 06:20:13 -0300

    testing

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
-
+from .models import Administrador, Cargo, Casos, DjangoContentType, DjangoMigrations, EvaluacionCaso, Evaluado, Evaluador, Persona, Resultado
 # Register your models here.
+
+admin.site.register(Administrador)
+admin.site.register(Cargo)
+admin.site.register(Casos)
+admin.site.register(DjangoContentType)
+admin.site.register(DjangoMigrations)
+admin.site.register(EvaluacionCaso)
+admin.site.register(Evaluado)
+admin.site.register(Evaluador)
+admin.site.register(Persona)
+admin.site.register(Resultado)
