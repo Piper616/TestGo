244615863d1b5c4d8fa36aacfbd61ecc3a891b99
Piper616 <f.diazmendez@gmail.com>
2021-10-06 20:09:59 -0300

    Arreglos

--- home/views.py
+++ home/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
-from .models import Administrador, Cargo, Casos, DjangoContentType, DjangoMigrations, EvaluacionCaso, Evaluado, Evaluador, Persona, Resultado  
+from .models import Administrador, Cargo, Casos, DjangoContentType, DjangoMigrations, EvaluacionCaso, Evaluado, Evaluador, Persona, Resultado
 import random
 from random import randint
 
@@ -12,18 +12,13 @@ def seleccion(request):
     return render(request,'home/seleccion.html')
 
 def video(request):
-    return render(request,'home/video.html') 
+    return render(request,'home/video.html')
 
 def foto(request):
     return render(request,'home/foto.html')
 
 def cuestionario(request):
-    return render(request,'home/cuestionario.html')    
+    return render(request,'home/cuestionario.html')
 
 def final(request):
-    return render(request, 'home/final.html')     
-
-def fotoaleatoria(request):
-    fotoa = ['/static/home/img/foto1.jpg', '/static/home/img/foto2.jpg']
-    random.sample(fotoa, 1)
-    return render(request, 'home/foto.html')
+    return render(request, 'home/final.html')
