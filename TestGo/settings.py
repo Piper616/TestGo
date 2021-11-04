b91f9d96301aa74e9cf9aea0b46a48912389ac92
Piper616 <f.diazmendez@gmail.com>
2021-11-04 17:30:36 -0300

    Version buena del proyecto

--- TestGo/settings.py
+++ TestGo/settings.py
@@ -85,9 +85,16 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),   
-    }
+        'ENGINE': 'django.db.backends.oracle',
+        'NAME': '127.0.0.1:1521/xe',
+        'USER': 'chandelle',
+        'PASSWORD': '123',
+        'TEST': {
+            'USER': 'default_test',
+            'TBLSPACE': 'default_test_tbls',
+            'TBLSPACE_TMP': 'default_test_tbls_tmp',
+        },
+    },
 }
 
 
