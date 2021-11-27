7db9bbe80b774da42174aaa6cbfdcf7739d14ea1
Piper616 <f.diazmendez@gmail.com>
2021-11-27 00:20:03 -0300

    ajustes para el despliegue

--- TestGo/settings.py
+++ TestGo/settings.py
@@ -18,7 +18,7 @@ from pathlib import Path
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -28,9 +28,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-_-bsld^4q0ed9w5u2&1$+pg5#r!vva-0#x381y6x7n)+-rq8s0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
