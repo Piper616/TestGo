132db949a8dcb4dda4e22c3bca5a46508a1cddb0
Piper616 <f.diazmendez@gmail.com>
2021-11-30 14:45:14 -0300

    cambios bd

--- home/admin.py
+++ home/admin.py
@@ -1,15 +1,4 @@
 from django.contrib import admin
-from .models import (Administrador,
-                             Cargo,
-                             Casos,
-                    EvaluacionCaso,
-                          Evaluado,
-                         Evaluador)
+
 # Register your models here.
 
-admin.site.register(Administrador)
-admin.site.register(Cargo)
-admin.site.register(Casos)
-admin.site.register(EvaluacionCaso)
-admin.site.register(Evaluado)
-admin.site.register(Evaluador)
